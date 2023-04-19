0c5ac408d907a7da575a5d42ad0104f56d4c3ae2
saki <saki.okatani1@gmail.com>
2023-04-19 03:41:33 -0300

    rotas usuario atualizadas, delete funcionando

--- Modulo1/Semana_09/src/routes/user_routes.js
+++ Modulo1/Semana_09/src/routes/user_routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 // const bodyParser = require('body-parser');
 const { handleUserCreation } = require('./user_routes/user_create');
+const { handleUserDeletion } = require('./user_routes/user_delete');
 
 
 
@@ -14,6 +15,8 @@ router.get('/', (req, res) => {
     res.status(200).json({message: 'connection to root has been established.'});
 });
 
-router.post('/user', handleUserCreation);
+router.post('/users', handleUserCreation);
+
+router.delete('/users/:id', handleUserDeletion);
     
 module.exports = router;
